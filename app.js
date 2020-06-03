4a26795a7be8e0c745685ef1c71b7673086f9dcf
ashp24091999 <ashp24091999@gmail.com>
2020-06-03 19:56:08 +0530

    webinar 2

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express=require('express')
+var bodyparser=require("body-parser")
 const server=express()
+var data=[
+    {Name:"Ram",Age:12,Sub:"Maths"},
+    {Name:"John",Age:13,Sub:"Chemistry"},
+    {Name:"Seeta",Age:14,Sub:"Physics"}]
+server.use(bodyparser.urlencoded({extended:true}))
 server.set("view engine","ejs")
 
 server.get("/",function(req,res){
@@ -18,9 +24,17 @@ server.get("/aboutus",function(req,res){
 server.get("/services",function(req,res){
     res.render("services") 
  })
- server.get("/validate",function(req,res){
-    res.render("home") 
+ server.post("/validate",function(req,res){
+     var user=req.body.userid;
+     var password=req.body.pass;
+     console.log(password)
+     if(user=="admin"&&password=="pass@123"){
+         res.render("home",{students:data})
+     }
+     else{
+    res.render("login")
+     } 
  })
-server.listen(3000,function(){
+server.listen(process.env.PORT|3000,function(){
     console.log("server started listening..")
 })
